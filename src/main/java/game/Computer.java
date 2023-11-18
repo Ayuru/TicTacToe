d1d459e569a84560f642d7d6b8b04981660893ea
Ayu <ayuru.gi@gmail.com>
2023-11-18 11:32:22 +0100

    computer ai fix

--- src/main/java/game/Computer.java
+++ src/main/java/game/Computer.java
@@ -87,45 +87,57 @@ public class Computer {
 
 
     public boolean betweenEast(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourEast().neighbourEast()) && !gameLogic.availabilityCheck(move.neighbourEast(), players);
+        return player.getMoves().contains(move.neighbourEast().neighbourEast())
+                && !gameLogic.availabilityCheck(move.neighbourEast(), players);
     }
 
     public boolean betweenWest(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourWest().neighbourWest()) && !gameLogic.availabilityCheck(move.neighbourWest(), players);
+        return player.getMoves().contains(move.neighbourWest().neighbourWest())
+                && !gameLogic.availabilityCheck(move.neighbourWest(), players);
     }
 
     public boolean betweenNorth(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourNorth().neighbourNorth()) && !gameLogic.availabilityCheck(move.neighbourNorth(), players);
+        return player.getMoves().contains(move.neighbourNorth().neighbourNorth())
+                && !gameLogic.availabilityCheck(move.neighbourNorth(), players);
     }
 
     public boolean betweenSouth(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourSouth().neighbourSouth()) && !gameLogic.availabilityCheck(move.neighbourSouth(), players);
+        return player.getMoves().contains(move.neighbourSouth().neighbourSouth())
+                && !gameLogic.availabilityCheck(move.neighbourSouth(), players);
     }
 
     public boolean betweenNW(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourNW().neighbourNW()) && !gameLogic.availabilityCheck(move.neighbourNW(), players);
+        return player.getMoves().contains(move.neighbourNW().neighbourNW())
+                && !gameLogic.availabilityCheck(move.neighbourNW(), players);
     }
 
     public boolean betweenNE(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourNE().neighbourNE()) && !gameLogic.availabilityCheck(move.neighbourNE(), players);
+        return player.getMoves().contains(move.neighbourNE().neighbourNE())
+                && !gameLogic.availabilityCheck(move.neighbourNE(), players);
     }
 
     public boolean betweenSW(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourSW().neighbourSW()) && !gameLogic.availabilityCheck(move.neighbourSW(), players);
+        return player.getMoves().contains(move.neighbourSW().neighbourSW())
+                && !gameLogic.availabilityCheck(move.neighbourSW(), players);
     }
 
     public boolean betweenSE(Players players, Player player, GameLogic gameLogic, Coordinates move) {
-        return player.getMoves().contains(move.neighbourNW().neighbourNW()) && !gameLogic.availabilityCheck(move.neighbourNW(), players);
+        return player.getMoves().contains(move.neighbourNW().neighbourNW())
+                && !gameLogic.availabilityCheck(move.neighbourNW(), players);
     }
 
     public Coordinates nextToRow(Players players, Player player, GameLogic gameLogic, Coordinates move, int boardSize) {
-        if (player.getMoves().contains(move.neighbourWest()) && !gameLogic.availabilityCheck(move.neighbourWest().neighbourWest(), players) && !gameLogic.rangeCheck(move.neighbourWest().neighbourWest(), boardSize)) {
+        if (player.getMoves().contains(move.neighbourWest())
+                && isEmpty(players, gameLogic, move.neighbourWest().neighbourWest(), boardSize)) {
             return move.neighbourWest().neighbourWest();
-        } else if (player.getMoves().contains(move.neighbourWest()) && !gameLogic.availabilityCheck(move.neighbourEast(), players) && !gameLogic.rangeCheck(move.neighbourEast(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourWest())
+                && isEmpty(players, gameLogic, move.neighbourEast(), boardSize)) {
             return move.neighbourEast();
-        } else if (player.getMoves().contains(move.neighbourEast()) && !gameLogic.availabilityCheck(move.neighbourEast().neighbourEast(), players) && !gameLogic.rangeCheck(move.neighbourEast().neighbourEast(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourEast())
+                && isEmpty(players, gameLogic, move.neighbourEast().neighbourEast(), boardSize)) {
             return move.neighbourEast().neighbourEast();
-        } else if (player.getMoves().contains(move.neighbourEast()) && !gameLogic.availabilityCheck(move.neighbourWest(), players) && !gameLogic.rangeCheck(move.neighbourWest(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourEast())
+                && isEmpty(players, gameLogic, move.neighbourWest(), boardSize)) {
             return move.neighbourWest();
         } else {
             return null;
@@ -133,13 +145,17 @@ public class Computer {
     }
 
     public Coordinates nextToColumn(Players players, Player player, GameLogic gameLogic, Coordinates move, int boardSize) {
-        if (player.getMoves().contains(move.neighbourNorth()) && !gameLogic.availabilityCheck(move.neighbourNorth().neighbourNorth(), players) && !gameLogic.rangeCheck(move.neighbourNorth().neighbourNorth(), boardSize)) {
+        if (player.getMoves().contains(move.neighbourNorth())
+                && isEmpty(players, gameLogic, move.neighbourNorth().neighbourNorth(), boardSize)) {
             return move.neighbourNorth().neighbourNorth();
-        } else if (player.getMoves().contains(move.neighbourNorth()) && !gameLogic.availabilityCheck(move.neighbourSouth(), players) && !gameLogic.rangeCheck(move.neighbourSouth(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourNorth())
+                && isEmpty(players, gameLogic, move.neighbourSouth(), boardSize)) {
             return move.neighbourSouth();
-        } else if (player.getMoves().contains(move.neighbourSouth()) && !gameLogic.availabilityCheck(move.neighbourSouth().neighbourSouth(), players) && !gameLogic.rangeCheck(move.neighbourSouth().neighbourSouth(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourSouth())
+                && isEmpty(players, gameLogic, move.neighbourSouth().neighbourSouth(), boardSize)) {
             return move.neighbourSouth().neighbourSouth();
-        } else if (player.getMoves().contains(move.neighbourSouth()) && !gameLogic.availabilityCheck(move.neighbourNorth(), players) && !gameLogic.rangeCheck(move.neighbourNorth(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourSouth())
+                && isEmpty(players, gameLogic, move.neighbourNorth(), boardSize)) {
             return move.neighbourNorth();
         } else {
             return null;
@@ -147,13 +163,17 @@ public class Computer {
     }
 
     public Coordinates nextToCrossRight(Players players, Player player, GameLogic gameLogic, Coordinates move, int boardSize) {
-        if (player.getMoves().contains(move.neighbourNW()) && !gameLogic.availabilityCheck(move.neighbourNW().neighbourNW(), players) && !gameLogic.rangeCheck(move.neighbourNW().neighbourNW(), boardSize)) {
+        if (player.getMoves().contains(move.neighbourNW())
+                && isEmpty(players, gameLogic, move.neighbourNW().neighbourNW(), boardSize)) {
             return move.neighbourNW().neighbourNW();
-        } else if (player.getMoves().contains(move.neighbourNW()) && !gameLogic.availabilityCheck(move.neighbourSE(), players) && !gameLogic.rangeCheck(move.neighbourSE(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourNW())
+                && isEmpty(players, gameLogic, move.neighbourSE(), boardSize)) {
             return move.neighbourSE();
-        } else if (player.getMoves().contains(move.neighbourSE()) && !gameLogic.availabilityCheck(move.neighbourSE().neighbourSE(), players) && !gameLogic.rangeCheck(move.neighbourSE().neighbourSE(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourSE())
+                && isEmpty(players, gameLogic, move.neighbourSE().neighbourSE(), boardSize)) {
             return move.neighbourSE().neighbourSE();
-        } else if (player.getMoves().contains(move.neighbourSE()) && !gameLogic.availabilityCheck(move.neighbourNW(), players) && !gameLogic.rangeCheck(move.neighbourNW(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourSE())
+                && isEmpty(players, gameLogic, move.neighbourNW(), boardSize)) {
             return move.neighbourNW();
         } else {
             return null;
@@ -161,17 +181,25 @@ public class Computer {
     }
 
     public Coordinates nextToCrossLeft(Players players, Player player, GameLogic gameLogic, Coordinates move, int boardSize) {
-        if (player.getMoves().contains(move.neighbourNE()) && !gameLogic.availabilityCheck(move.neighbourNE().neighbourNE(), players) && !gameLogic.rangeCheck(move.neighbourNE().neighbourNE(), boardSize)) {
+        if (player.getMoves().contains(move.neighbourNE())
+                && isEmpty(players, gameLogic, move.neighbourNE().neighbourNE(), boardSize)) {
             return move.neighbourNE().neighbourNE();
-        } else if (player.getMoves().contains(move.neighbourNE()) && !gameLogic.availabilityCheck(move.neighbourSW(), players) && !gameLogic.rangeCheck(move.neighbourSW(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourNE())
+                && isEmpty(players, gameLogic, move.neighbourSW(), boardSize)) {
             return move.neighbourSW();
-        } else if (player.getMoves().contains(move.neighbourSW()) && !gameLogic.availabilityCheck(move.neighbourSW().neighbourSW(), players) && !gameLogic.rangeCheck(move.neighbourSW().neighbourSW(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourSW())
+                && isEmpty(players, gameLogic, move.neighbourSW().neighbourSW(), boardSize)) {
             return move.neighbourSW().neighbourSW();
-        } else if (player.getMoves().contains(move.neighbourSW()) && !gameLogic.availabilityCheck(move.neighbourNE(), players) && !gameLogic.rangeCheck(move.neighbourNE(), boardSize)) {
+        } else if (player.getMoves().contains(move.neighbourSW())
+                && isEmpty(players, gameLogic, move.neighbourNE(), boardSize)) {
             return move.neighbourNE();
         } else {
             return null;
         }
+    }
+
+    public boolean isEmpty(Players players, GameLogic gameLogic, Coordinates move, int boardSize) {
+        return !gameLogic.availabilityCheck(move, players) && !gameLogic.rangeCheck(move, boardSize);
     }
 
 }
